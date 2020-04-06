5e26b76c2bfd2d8706606fd8e85f9020c17f2009
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-04-05 21:20:39 -0300

    fixed port 8001

--- chatbot/utils/CheckFake.js
+++ chatbot/utils/CheckFake.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 function checkFake(texto) {
 	return new Promise((resolve, reject) => {
-		request.post('http://hackforgood-fakedtc:8001/check', {
+		request.post('http://ip172-18-0-7-bq5598roudsg00b7sv9g-8001.direct.labs.play-with-docker.com/check', {
 			json: {
 				text: texto
 			}
