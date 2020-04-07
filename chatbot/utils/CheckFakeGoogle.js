92d30b07dc096877171b45fbfbe6a5b44b8f1c59
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-04-06 22:20:17 -0300

    stopwords new

--- chatbot/utils/CheckFakeGoogle.js
+++ chatbot/utils/CheckFakeGoogle.js
@@ -1,7 +1,7 @@
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 
-var stopwords = ["vai", "se", "nem", "suas", "forma", "de", "governo", "as", "mentes", "tudo", "culpa", "da", "emissora", "da", "or", "liderar", "sou", "a", "liderança", "logo", "expedição", "hope", "par", "chegar", "coisa", "outra", "último", "é", "acerca", "agora", "algmas", "alguns", "ali", "ambos", "antes", "apontar", "aquela", "aquelas", "aquele", "aqueles", "aqui", "atrás", "bem", "bom", "cada", "caminho", "cima", "com", "como", "comprido", "conhecido", "corrente", "das", "debaixo", "dentro", "desde", "desligado", "deve", "devem", "deverá", "direita", "diz", "dizer", "dois", "dos", "e", "ela", "ele", "eles", "em", "enquanto", "então", "está", "estão", "estado", "estar 	estará", "este", "estes", "esteve", "estive", "estivemos", "estiveram", "eu", "fará", "faz", "fazer", "fazia", "fez", "fim", "foi", "fora", "horas", "iniciar", "inicio", "ir", "irá", "ista", "iste", "isto", "ligado", "maioria", "maiorias", "mais", "mas", "mesmo", "meu", "muito", "muitos", "nós", "não", "nome", "nosso", "novo", "o", "onde", "os", "ou", "outro", "para", "parte", "pegar", "pelo", "pessoas", "pode", "poderá 	podia", "por", "porque", "povo", "promeiro", "quê", "qual", "qualquer", "quando", "quem", "quieto", "são", "saber", "sem", "ser", "seu", "somente", "têm", "tal", "também", "tem", "tempo", "tenho", "tentar", "tentaram", "tente", "tentei", "teu", "teve", "tipo", "tive", "todos", "trabalhar", "trabalho", "tu", "um", "uma", "umas", "uns", "usa", "usar", "valor", "veja", "ver", "verdade", "verdadeiro", "você"]
+var stopwords = ["globo", "crianças", "vai", "se", "nem", "suas", "forma", "de", "governo", "as", "mentes", "tudo", "culpa", "da", "emissora", "da", "or", "liderar", "sou", "a", "liderança", "logo", "expedição", "hope", "par", "chegar", "coisa", "outra", "último", "é", "acerca", "agora", "algmas", "alguns", "ali", "ambos", "antes", "apontar", "aquela", "aquelas", "aquele", "aqueles", "aqui", "atrás", "bem", "bom", "cada", "caminho", "cima", "com", "como", "comprido", "conhecido", "corrente", "das", "debaixo", "dentro", "desde", "desligado", "deve", "devem", "deverá", "direita", "diz", "dizer", "dois", "dos", "e", "ela", "ele", "eles", "em", "enquanto", "então", "está", "estão", "estado", "estar 	estará", "este", "estes", "esteve", "estive", "estivemos", "estiveram", "eu", "fará", "faz", "fazer", "fazia", "fez", "fim", "foi", "fora", "horas", "iniciar", "inicio", "ir", "irá", "ista", "iste", "isto", "ligado", "maioria", "maiorias", "mais", "mas", "mesmo", "meu", "muito", "muitos", "nós", "não", "nome", "nosso", "novo", "o", "onde", "os", "ou", "outro", "para", "parte", "pegar", "pelo", "pessoas", "pode", "poderá 	podia", "por", "porque", "povo", "promeiro", "quê", "qual", "qualquer", "quando", "quem", "quieto", "são", "saber", "sem", "ser", "seu", "somente", "têm", "tal", "também", "tem", "tempo", "tenho", "tentar", "tentaram", "tente", "tentei", "teu", "teve", "tipo", "tive", "todos", "trabalhar", "trabalho", "tu", "um", "uma", "umas", "uns", "usa", "usar", "valor", "veja", "ver", "verdade", "verdadeiro", "você"]
 
 function checkFake(texto) {
 	return new Promise(async (resolve, reject) => {
@@ -16,13 +16,20 @@ function checkFake(texto) {
 			dateRestrict: "d10",
 			num: 2,
 		})
+		var alternativas = ""
+		if (response.data.items == undefined) {
+			alternativas = {
+				message: "Clique aqui para obter mais detalhes:\n\nhttps://www.google.com/search?q=" + texto
+			}
+		} else {
+			alternativas = response.data.items.map((item) => {
+				var message = ""
+				message += item.snippet
+				message += "\n\n" + item.link
+				return { message }
+			})
+		}
 
-		var alternativas = response.data.items.map((item) => {
-			var message = ""
-			message += item.snippet
-			message += "\n\n" + item.link
-			return { message }
-		})
 		resolve(alternativas)
 	})
 }
