fa97772ae27015b5a0261f44c7da8cf51ed2a5aa
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-04-06 22:08:30 -0300

    no data restrictionn

--- chatbot/utils/CheckFakeGoogle.js
+++ chatbot/utils/CheckFakeGoogle.js
@@ -1,13 +1,18 @@
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 
+var stopwords = ["liderar", "sou", "a", "liderança", "logo", "expedição", "hope", "par", "chegar", "coisa", "outra", "último", "é", "acerca", "agora", "algmas", "alguns", "ali", "ambos", "antes", "apontar", "aquela", "aquelas", "aquele", "aqueles", "aqui", "atrás", "bem", "bom", "cada", "caminho", "cima", "com", "como", "comprido", "conhecido", "corrente", "das", "debaixo", "dentro", "desde", "desligado", "deve", "devem", "deverá", "direita", "diz", "dizer", "dois", "dos", "e", "ela", "ele", "eles", "em", "enquanto", "então", "está", "estão", "estado", "estar 	estará", "este", "estes", "esteve", "estive", "estivemos", "estiveram", "eu", "fará", "faz", "fazer", "fazia", "fez", "fim", "foi", "fora", "horas", "iniciar", "inicio", "ir", "irá", "ista", "iste", "isto", "ligado", "maioria", "maiorias", "mais", "mas", "mesmo", "meu", "muito", "muitos", "nós", "não", "nome", "nosso", "novo", "o", "onde", "os", "ou", "outro", "para", "parte", "pegar", "pelo", "pessoas", "pode", "poderá 	podia", "por", "porque", "povo", "promeiro", "quê", "qual", "qualquer", "quando", "quem", "quieto", "são", "saber", "sem", "ser", "seu", "somente", "têm", "tal", "também", "tem", "tempo", "tenho", "tentar", "tentaram", "tente", "tentei", "teu", "teve", "tipo", "tive", "todos", "trabalhar", "trabalho", "tu", "um", "uma", "umas", "uns", "usa", "usar", "valor", "veja", "ver", "verdade", "verdadeiro", "você"]
+
 function checkFake(texto) {
 	return new Promise(async (resolve, reject) => {
-		console.log('checkFake', texto.substring(0, 150))
+
+		texto = remove_stopwords(texto.substring(0, 150) + ".")
+		console.log('checkFake', texto)
+
 		const response = await customSearch.cse.list({
 			auth: process.env.googleSearchApiKey,
 			cx: process.env.googleSearchId,
-			q: texto.substring(0, 150),
+			q: texto,
 			// dateRestrict: "d10",
 			num: 2,
 		})
@@ -20,6 +25,18 @@ function checkFake(texto) {
 		})
 		resolve(alternativas)
 	})
+}
+
+function remove_stopwords(str) {
+	res = []
+	words = str.split(' ')
+	for (i = 0; i < words.length; i++) {
+		word_clean = words[i].split(".").join("")
+		if (!stopwords.includes(word_clean)) {
+			res.push(word_clean)
+		}
+	}
+	return (res.join(' '))
 }
 
 module.exports = {
